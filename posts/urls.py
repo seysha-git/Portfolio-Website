25b9e7b245d7267f298aaeedc593be19f5e11809
Seyon Shanthan <seyontennis@gmail.com>
2024-06-18 21:01:12 +0200

    Chapter 6

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.posts_lists)
+    path('', views.posts_lists, name="list"),
+    path('<slug:slug>', views.post_page, name="page"),
 ]
+
