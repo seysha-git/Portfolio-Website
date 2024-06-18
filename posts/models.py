c9746eb39ec98fdedff03556f5860003da058289
Seyon Shanthan <seyontennis@gmail.com>
2024-06-18 19:42:17 +0200

    Latest change

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=75, default="SOME STRING")
+    body = models.TextField(default="some string")
+    slug = models.SlugField(default="some string")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 def posts_lists(request):
-    return render(request, "posts/posts_list.html")
+    posts = Post.objects.all().order_by('-date')
+
+    return render(
+        request, 
+        "posts/posts_list.html", 
+        {'posts': posts}
+    )
